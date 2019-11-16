fa807f4bc34f2a7c04e13cbe338df1214fd21d2c
francois.moine <francois.moine@epitech.eu>
2019-11-16 03:16:14 +0100

    create constants for API & routes

---
+++ src/constants/index.js
@@ -0,0 +1,20 @@
+export const ROOT = '/';
+
+export const SEARCH = ROOT + 'search';
+export const DETAIL = ROOT + 'detail';
+
+export const API_KEY = '6dcb54a8f06bb6506f9df08eefb9b46f';
+
+export const FETCH_MOVIE = '/search/movie';
+
+export const URI = 'https://api.themoviedb.org/3';
+export const URL_IMG = 'https://image.tmdb.org/t/p/w500_and_h282_face';
+
+
+// Request
+export const US_REQ = 'en-US';
+export const API_KEY_REQ = 'api_key';
+export const LANGUAGE_REQ = 'language';
+export const PAGE_REQ = 'page';
+export const ADULT_REQ = 'include_adult';
+export const QUERY_REQ = 'query';
