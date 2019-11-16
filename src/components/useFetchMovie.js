dbff7d6a49c24bab0ba5524204d8592914f7e3f3
francois.moine <francois.moine@epitech.eu>
2019-11-16 17:12:44 +0100

    get detail information from id_movie

--- src/components/useFetchMovie.js
+++ src/components/useFetchMovie.js
@@ -4,31 +4,63 @@ import React from 'react';
 import { initRequest, createRequest } from './utils';
 
 // Constants
-import { URI, FETCH_MOVIE, QUERY_REQ } from '../constants';
+import { URI, FETCH_MOVIE, QUERY_REQ, ADULT_REQ } from '../constants';
+import { MOVIE_DETAIL } from '../constants';
+
+export const fetchApi = async (uri, setResponse) => {
+   // console.log("URI", uri);
+   const response = await fetch(uri);
+   if (!response.ok) {
+      setResponse(null)
+      // console.error(response);
+      return response;
+   }
+   // console.warn("Response :", response);
+   const json = await response.json();
+   // console.log("JSON: ", json);
+   setResponse(json);
+   return response;
+}
 
 export const useFetchMovies = (props) => {
    const { search, fetchSearch } = props;
    const [movies, setMovies] = React.useState(null);
 
    React.useEffect(() => {
-      const fetchMovie = async () => {
-         if (!search)
-            return setMovies(null);
-         const request = {
-            ...initRequest,
-            [QUERY_REQ]: search
-         }
-         const uri = createRequest(URI + FETCH_MOVIE, request);
-         console.log("URI", uri);
-         const response = await fetch(uri);
-         // console.warn("Response :", response);
-         const json = await response.json();
-         // console.log("JSON: ", json);
-         setMovies(json);
-      }
+      if (!search)
+         return setMovies(null);
+      const request = {
+         ...initRequest,
+         [QUERY_REQ]: search,
+         [ADULT_REQ]: false,
+      };
+      const url = URI + FETCH_MOVIE;
+      const uri = createRequest(url, request);
       // console.log("Response :", fetchMovie());
-      fetchMovie();
+      fetchApi(uri, setMovies)
+         .catch(err => console.error(err));
    }, [fetchSearch]);
 
    return (movies);
-}
+};
+
+export const useFetchMovieById = (props) => {
+   const { id } = props;
+   const [movie, setMovie] = React.useState(null);
+
+   React.useEffect(() => {
+      if (!id)
+         setMovie(null);
+      const request = {
+         ...initRequest,
+      }
+      const url = URI + MOVIE_DETAIL + '/' + id;
+      const uri = createRequest(url, request);
+      console.log("URI :", uri);
+      fetchApi(uri, setMovie)
+         .catch(err => console.log(err));
+   }, [id]);
+
+   return (movie);
+};
+
