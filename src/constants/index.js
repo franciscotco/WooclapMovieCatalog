9fab1d1c7dda2de33737c73fa1fe5eca33f74c4e
francois.moine <francois.moine@epitech.eu>
2019-11-16 17:13:29 +0100

    rename constants folder and add routes

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,16 +1,20 @@
 export const ROOT = '/';
 
-export const SEARCH = ROOT + 'search';
-export const DETAIL = ROOT + 'detail';
+export const SEARCH = '/search';
+export const ID_MOVIE = 'id_movie'
+export const DETAIL = '/detail/:' + ID_MOVIE;
+
+export const MOVIE = '/detail'
 
 export const API_KEY = '6dcb54a8f06bb6506f9df08eefb9b46f';
 
 export const FETCH_MOVIE = '/search/movie';
 
+
 export const URI = 'https://api.themoviedb.org/3';
 export const URL_IMG = 'https://image.tmdb.org/t/p/w500_and_h282_face';
 
-
+export const MOVIE_DETAIL = '/movie'
 // Request
 export const US_REQ = 'en-US';
 export const API_KEY_REQ = 'api_key';
