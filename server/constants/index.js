4470b15dce4081f89c2017018f5ed2b08026b5f9
francois.moine <francois.moine@epitech.eu>
2019-11-18 19:38:56 +0100

    create constants

---
+++ server/constants/index.js
@@ -0,0 +1,5 @@
+export const TITLE = 'original_title';
+export const POSTER_PATH = 'poster_path';
+export const RATINGS = 'vote_average';
+export const GENRES = 'genres';
+export const DATE = 'release_date';
